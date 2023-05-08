a118906f77a8b962a7c4a2a4e9dcf259f8287666
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-05-08 14:59:08 +0530

    1st

---
+++ models/Ticket.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const moment = require('moment-timezone');
+const timeZone = moment.tz(Date.now(),"Asia/Kolkata");
+const Schema = mongoose.Schema
+
+// Create Schema
+const messages = new mongoose.Schema({
+    text: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: timeZone,
+
+    },
+    files: {
+        type: Array
+    }
+})
+
+
+const TicketSchema = new Schema({
+    name: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: timeZone,
+    
+    },
+    content: {
+        type: String
+    },
+    messages: {
+        type: Array, 
+        default: []
+    },
+    status: {
+        type: Number,
+        default: 0
+    },
+    img: {
+        type:String
+    },
+ 
+
+})
+
+module.exports = User = mongoose.model('ticket', TicketSchema)
